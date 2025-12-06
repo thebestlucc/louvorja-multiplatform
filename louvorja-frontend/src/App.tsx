a7bda262cc13978cc114cf0330e14ae55f0ddcdb
thebestlucc <thebestlucc@gmail.com>
2025-12-06 14:02:34 -0300

    feat: start `Tauri` project and adds `tailwindcss` v4 to project

---
+++ louvorja-frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import { Button } from "./button.tsx";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <h1 className="text-3xl font-bold text-amber-400">LouvorJA</h1>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <Button type="submit">Greet</Button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
